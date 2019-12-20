51acc85013870391d765aeab5a662d76e1118a81
Samson <samson.charlonnai@hotmail.com>
2019-12-20 15:20:27 +0100

    yy

--- src/main/java/fr/dawan/projettest/Controllers/LoginController.java
+++ src/main/java/fr/dawan/projettest/Controllers/LoginController.java
@@ -1,8 +1,87 @@
+//package fr.dawan.projettest.Controllers;
+//
+//import javax.servlet.http.HttpSession;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.support.SessionStatus;
+//
+//import fr.dawan.projettest.DAO.UtilisateurDAO;
+//import fr.dawan.projettest.entite.Utilisateur;
+//import fr.dawan.projettest.service.UtilisateurService;
+//
+//@Controller
+//public class LoginController {
+//	
+//	@Autowired
+//	private UtilisateurService service;
+//	
+//	@GetMapping("/login")
+//	public String login() {
+//
+//		return "login";
+//	}
+//	
+//	@GetMapping("/inscription")
+//	public String inscription() {
+//		
+//		return "inscription";
+//	}
+//
+//	
+//	@PostMapping("/login/authentification")
+//	public String authentification(Model model, @RequestParam("login") String login, @RequestParam("pass") String pass,
+//			HttpSession session) {
+//		
+//	    Utilisateur util = service.findUserByEmailAndPwd(login, pass); 
+//	    
+//	    System.out.println(util);
+//	    
+//		String pseudo = util.getPseudo();
+//		String email = util.getEmail();
+//		String mdp = util.getMdp();
+//		
+//		if ((pseudo.equals(login) || email.equals(login)) && mdp.equals(pass)) {
+//			session.setAttribute("login", login);
+//			return "welcome";
+//		}else {
+//			model.addAttribute("msg", "Erreur d'authentification!!!");
+//			return "login";
+//		}
+//		
+//		
+//
+////		if (login.equals("admin") && pass.equals("admin")) {
+////			session.setAttribute("login", login);
+////			return "welcome";
+////		} else {
+////			model.addAttribute("msg", "Erreur d'authentification!!!");
+////			return "login";
+////		}
+//
+//	}
+//	
+//	
+//	
+//	@GetMapping("/login/deconnexion")
+//	public String logout(HttpSession session, SessionStatus status) {
+//
+//		// status.setComplete();
+//		session.invalidate();
+//
+//		return "home";
+//	}
+//	
+//}
+
 package fr.dawan.projettest.Controllers;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +89,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
 
-import fr.dawan.projettest.DAO.UtilisateurDAO;
-import fr.dawan.projettest.entite.Utilisateur;
-import fr.dawan.projettest.service.UtilisateurService;
-
 @Controller
 public class LoginController {
-	
-	@Autowired
-	private UtilisateurService service;
 	
 	@GetMapping("/login")
 	public String login() {
@@ -33,35 +105,17 @@ public class LoginController {
 	}
 
 	
-	@PostMapping("/login/authentification")
+	@PostMapping("/connexion/authentification")
 	public String authentification(Model model, @RequestParam("login") String login, @RequestParam("pass") String pass,
 			HttpSession session) {
-		
-	    Utilisateur util = service.findUserByEmailAndPwd(login, pass); 
-	    
-	    System.out.println(util);
-	    
-		String pseudo = util.getPseudo();
-		String email = util.getEmail();
-		String mdp = util.getMdp();
-		
-		if ((pseudo.equals(login) || email.equals(login)) && mdp.equals(pass)) {
+
+		if (login.equals("admin") && pass.equals("admin")) {
 			session.setAttribute("login", login);
 			return "welcome";
-		}else {
+		} else {
 			model.addAttribute("msg", "Erreur d'authentification!!!");
 			return "login";
 		}
-		
-		
-
-//		if (login.equals("admin") && pass.equals("admin")) {
-//			session.setAttribute("login", login);
-//			return "welcome";
-//		} else {
-//			model.addAttribute("msg", "Erreur d'authentification!!!");
-//			return "login";
-//		}
 
 	}
 	
@@ -77,3 +131,4 @@ public class LoginController {
 	}
 	
 }
+
